99a82460eeff99443d1f756627daf55c29280967
JherannKaryllePascual <145414565+JherannKaryllePascual@users.noreply.github.com>
2023-10-06 05:58:07 +0530

    Add files via upload

---
+++ activity.js
@@ -0,0 +1,9 @@
+let firstFavNumber = 21;
+let secondFavNumber =  6;
+console.log(firstFavNumber + secondFavNumber);
+
+console.log(firstFavNumber - secondFavNumber);
+
+console.log(firstFavNumber * secondFavNumber);
+
+console.log(firstFavNumber / secondFavNumber);
---
+++ activty2.js
@@ -0,0 +1,11 @@
+let firstFavNumb = 21;
+let secondFavNumb = 6;
+console.log(firstFavNumb<secondFavNumb);
+console.log(firstFavNumb>secondFavNumb);
+console.log(firstFavNumb<=secondFavNumb);
+console.log(firstFavNumb>=secondFavNumb);
+console.log(firstFavNumb===secondFavNumb);
+console.log(firstFavNumb==secondFavNumb);
+console.log(firstFavNumb!==secondFavNumb);
+console.log(firstFavNumb!=secondFavNumb);
+console.log(power);
