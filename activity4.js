09bcd2e0a190b65a425381916703f8e574e2005f
JherannKaryllePascual <145414565+JherannKaryllePascual@users.noreply.github.com>
2023-10-10 09:55:18 +0530

    Add files via upload

---
+++ activity4.js
@@ -0,0 +1,17 @@
+let fruit = "banana";
+switch(fruit){
+    case "Banana":
+        console.log("Banana is good!");
+    break;
+
+    case "Orange":
+        console.log("I am not fan of orange.");
+    break;
+
+    case "Apple":
+        console.log("How you like them apples?");
+    break;
+    
+    default:
+        console.log("I have never heard of that fruit.");
+}
---
+++ activity5.js
@@ -0,0 +1,5 @@
+console.log("Jherann Karylle V. Pascual");
+
+     for (let i = 0; i <= 1000; i++){
+       console.log(i);
+     }
